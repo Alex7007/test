463e5c3f5491fff35b6780227cb5de109aad41f9
Alex Bulakh <a.s.bulakh@gmail.com>
2023-06-02 01:27:44 +0300

    1
    1

---
+++ backend/index.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "mysql";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Ваш код работы с MySQL
+
+$conn->close();
+?>
